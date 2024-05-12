30bb1f9cf00e66e761749334761fa7715aefa0cd
Marce <101135405+Marce952@users.noreply.github.com>
2024-05-11 22:10:05 -0300

    Contact us

--- components/Contactanos.jsx
+++ components/Contactanos.jsx
@@ -13,7 +13,7 @@ const Contactanos = () => {
                 <p className='text-2xl'>Si quieres hacer que tu negocia crezca, contactanos</p>
             </div>
 
-            <div className='w-full flex items-center justify-around mt-16'>
+            <div className='w-full flex items-center justify-around mt-16 md:flex-row max-sm:flex-col max-sm:gap-8'>
                 <div className='flex flex-col justify-center items-center gap-8 min-w-[16rem] min-h-[10rem] text-black bg-white rounded-md shadow-[0_10px_20px_rgba(255,_0,_0,_0.7)]'>
                     <div>
                         <HiOutlineMail className='text-3xl' />
@@ -29,15 +29,15 @@ const Contactanos = () => {
                 </div>
             </div>
 
-            <div className='w-3/4 min-h-[13rem] bg-white rounded-md p-4'>
-                <div className="mx-auto max-w-2xl text-center">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Envianos un mensaje</h2>
+            <div className='w-3/4 min-h-[13rem] bg-white rounded-md p-4 relative  overflow-hidden'>
+                <div className="mx-auto max-w-2xl text-center z-10">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl z-10">Envianos un mensaje</h2>
                 </div>
 
-                <form className="mx-auto max-w-xl sm:mt-20">
-                    <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <form className="mx-auto max-w-xl mt-10 z-10">
+                    <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2 z-10">
                         <div>
-                            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900 z-10">
                                 Nombre completo
                             </label>
                             <div className="mt-2.5">
@@ -46,26 +46,26 @@ const Contactanos = () => {
                                     name="first-name"
                                     id="first-name"
                                     autoComplete="given-name"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md z-10 border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                            <label htmlFor="last-name" className="block text-sm z-10 font-semibold leading-6 text-gray-900">
                                 Email
                             </label>
                             <div className="mt-2.5">
                                 <input
-                                    type="text"
-                                    name="last-name"
+                                    type="email"
+                                    name="email"
                                     id="last-name"
                                     autoComplete="family-name"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full z-10 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
                         <div className="sm:col-span-2">
-                            <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+                            <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900 z-10">
                                 Mensaje
                             </label>
                             <div className="mt-2.5">
@@ -73,19 +73,24 @@ const Contactanos = () => {
                                     name="message"
                                     id="message"
                                     rows={4}
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full resize-none z-10 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     defaultValue={''}
                                 />
                             </div>
                         </div>
-                        <button
-                            type="submit"
-                            className="block w-full rounded-md bg-[#ff0000] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Enviar
-                        </button>
                     </div>
+                    <button
+                        type="submit"
+                        className="block w-full mt-2 mx-auto rounded-md bg-[#ff0000] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                    >
+                        Enviar
+                    </button>
                 </form>
+
+                {/* Divs con posición absoluta debajo del formulario */}
+                <div className='absolute w-[5rem] h-[5rem] rounded-full bg-[#ff0000] top-[-20px] left-[-20px] opacity-50'></div>
+                <div className='absolute w-[20rem] h-[20rem] rounded-full bg-[#ff0000] top-[50px] right-[-150px] opacity-50'></div>
+
             </div>
         </div>
     )
